c648ba0d2f730c6b812ad60c996dc3718b8c39ec
JuanaArenas <123477086+JuanaArenas@users.noreply.github.com>
2026-01-03 03:40:48 +0000

    octava

--- app/run.py
+++ app/run.py
@@ -175,7 +175,8 @@ def logout():
 # ========================
 # MAIN
 # ========================
-if __name__ == '__main__':
+if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
+
